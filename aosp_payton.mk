db4fc07fe69cf25b68332692078bc5b03aa9f245
Daniel Vásquez <danielgusvt@yahoo.com>
2019-04-10 00:12:45 +0000

    payton: Initial adaptation for AOSP
    * RiseOS does not have roomservice support, but still,
    let us change to aosp.dependencies :p
    
    Signed-off-by: Daniel Vásquez <danielgusvt@yahoo.com>

--- aosp_payton.mk
+++ aosp_payton.mk
@@ -16,8 +16,6 @@
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
@@ -68,7 +66,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := payton
-PRODUCT_NAME := lineage_payton
+PRODUCT_NAME := aosp_payton
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto X4
 PRODUCT_MANUFACTURER := Motorola
